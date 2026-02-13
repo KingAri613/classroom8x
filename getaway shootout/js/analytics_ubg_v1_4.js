fa7a05a0fa664649b8065acd4bd71e1d1e388608
KingAri613 <aryeh.gershlick2@gmail.com>
2026-02-13 17:00:08 +0000

    thjy

---
+++ getaway shootout/js/analytics_ubg_v1_4.js
@@ -0,0 +1,6 @@
+(function(){
+  var s = document.createElement('script');
+  s.src = '../analytics_ubg_v1_4.js';
+  s.async = false;
+  document.head.appendChild(s);
+})();
---
+++ getaway shootout/js/ubg235_client_v1_1.js
@@ -0,0 +1,6 @@
+(function(){
+  var s = document.createElement('script');
+  s.src = '../ubg235_client_v1_1.js';
+  s.async = false;
+  document.head.appendChild(s);
+})();
